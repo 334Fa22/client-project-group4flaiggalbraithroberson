cc49fda27e5547d83f46a162b7d8cc5c03a7e9e8
megangalbraith <s534205@nwmissouri.edu>
2022-10-13 18:32:47 -0500

    .

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,8 @@
 //Hamburger menu function
 function hamburger(){
 
+var menu = document.getEleentById("menu-links");
+var menu = document.getElementById("catpaws-logo");
 
   if(menu.style.display === "block" && logo.style.display === "none"){
     menu.style.display = "none";

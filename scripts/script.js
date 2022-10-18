d9657770fbdfb78401a2e95e146b856a5f4acde8
megangalbraith <s534205@nwmissouri.edu>
2022-10-18 13:25:48 -0500

    fixed hamburger menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,11 @@
 //Hamburger menu function
 function hamburger(){
 
-var menu = document.getEleentById("menu-links");
-var menu = document.getElementById("catpaws-logo");
+var menu = document.getElementById("menu-links");
 
-  if(menu.style.display === "block" && logo.style.display === "none"){
-    menu.style.display = "none";
-    logo.style.display = "block";
-  } else{
+  if(menu.style.display === "none"){
     menu.style.display = "block";
-    logo.style.display = "none";
+  } else{
+    menu.style.display = "none";
   }
 }
